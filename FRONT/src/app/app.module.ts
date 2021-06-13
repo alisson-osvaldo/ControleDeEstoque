a61f12c8e7d647a6ee56c5ad593c1e4fb3769c70
Alisson Osvaldo <alisson.artigas@gmail.com>
2021-06-13 00:39:41 -0300

    Adicionando Angular material

--- FRONT/src/app/app.module.ts
+++ FRONT/src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+
+
 import { AppComponent } from './app.component';
 import { ListarComponent } from './components/views/cadastroProdutos/listar/listar.component';
 import { CadastrarComponent } from './components/views/cadastroProdutos/cadastrar/cadastrar.component';
 import { ListarEntradaComponent } from './components/views/entrada/listar-entrada/listar-entrada.component';
 import { CadastrarEntradaComponent } from './components/views/entrada/cadastrar-entrada/cadastrar-entrada.component';
-
 import { HeaderComponent } from './components/template/header/header.component';
 import { ContentComponent } from './components/template/content/content.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './components/views/home/home.component';
 import { BuscarComponent } from './components/views/cadastroProdutos/buscar/buscar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 @NgModule({
@@ -26,14 +37,25 @@ import { BuscarComponent } from './components/views/cadastroProdutos/buscar/busc
     ListarEntradaComponent,
     CadastrarEntradaComponent,
     HomeComponent,
-    BuscarComponent
+    BuscarComponent,
+    
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
